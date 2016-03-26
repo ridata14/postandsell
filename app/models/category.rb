7e2e849538fad313ae4b0b69da522028cff12f06
Don Jason Gruspe <ridata14@gmail.com>
2016-03-27 01:48:44 +0800

    Added Fields for Postings

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
 
 	has_many :subcategories
+
+def to_s
+	name
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326114136) do
+ActiveRecord::Schema.define(version: 20160326170550) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20160326114136) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "subcategory_id"
+    t.string   "title"
+    t.integer  "price"
+    t.string   "condition"
+    t.string   "location"
+    t.text     "details"
+    t.string   "category"
+    t.integer  "contactnumber"
   end
 
   create_table "subcategories", force: :cascade do |t|
