5950d140ef241e7e0d3fc2f8bc5e8ba7d811bbdd
Don Jason Gruspe <ridata14@gmail.com>
2016-03-31 21:46:35 +0800

    added image feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,7 @@ end
 def show
 	@category = Category.find(params[:id])
 	@postings = Posting.where(category_id: params[:id])
-
+	
 end
 
 end
--- app/models/posting.rb
+++ app/models/posting.rb
@@ -4,4 +4,6 @@ class Posting < ActiveRecord::Base
 	belongs_to :category
 
 	has_many :photos
+
+	accepts_nested_attributes_for :photos
 end
